23f661a02750cabb0a83426add8f41709935850d
Konstantinos Stafylas <k.stafilas@kleemannlifts.com>
2023-09-20 18:10:06 +0300

    add login controller with the definition of login method

---
+++ MyTestProjectAPI/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MyTestProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLoginInfo userLogin)
+        {
+
+        }
+    }
+}
--- MyTestProjectAPI/Helpers/Data.cs
+++ MyTestProjectAPI/Helpers/Data.cs
@@ -1,6 +1,26 @@
-﻿namespace MyTestProjectAPI.Helpers
+﻿using MyTestProjectAPI.Models;
+using System.Collections.Generic;
+
+namespace MyTestProjectAPI.Helpers
 {
     public class Data
     {
+        public static List<User> Users = new List<User>()
+        {
+            new User()
+            {
+                Name = "Kostas",
+                Surname = "Papadopoulos",
+                Email = "exampleMail1@mail.gr",
+                Password = "123456789",
+            },
+             new User()
+            {
+                Name = "Nikos",
+                Surname = "Nikolaidis",
+                Email = "exampleMail2@mail.gr",
+                Password = "123456",
+            }
+        };
     }
 }
